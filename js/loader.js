c7a9cecd3f12670e611c31815e7c9d5f8f6fa0b2
mohamed <msa0back@gmail.com>
2024-03-18 15:32:06 +0200

    save

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    var loader = document.getElementById('loader-wrapper');
+    loader.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Re-enable scrolling
+});
